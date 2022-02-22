cf835c0c3aa428eb5ea767563ed3188fbc24b9f0
MaxKadema <75802419+MaxKadema@users.noreply.github.com>
2022-02-22 15:05:33 +0900

    check

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -1,17 +1,9 @@
-//
-//  StoryBrain.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import Foundation
 
 struct StoryBrain {
-    let storyArray = [/*Story(title: "You see a fork in the road.", choice1: "Take a left.", choice1Destination: 1, choice2: "Take a right.", choice2Destination: 2),
-                   Story(title: "You see a tiger.", choice1: "Shout for help.",choice1Destination: 0,  choice2: "Play dead.", choice2Destination: 0),
-                   Story(title: "You find a treasure chest.", choice1: "Open it.",choice1Destination: 0, choice2: "Check for traps.",choice2Destination: 0)*/
+    let storyArray = [
         Story(
                     title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.",
                     choice1: "I'll hop in. Thanks for the help!", choice1Destination: 2,
